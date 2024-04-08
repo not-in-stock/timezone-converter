e1de4c55fe328973f6792d232bcff9448b913c4b
Andrey Fedorov <oran9e.red@gmail.com>
2024-04-08 22:54:08 +0200

    Fix default styles

--- src/tz_converter/styles.cljs
+++ src/tz_converter/styles.cljs
@@ -3,12 +3,18 @@
    [spade.core   :refer [defglobal defclass]]))
 
 (defglobal defaults
-  [:body
-   {:background-size "cover"
-    :background-repeat :no-repeat
-    :font-family "Jost, sans-serif"
-    :font-optical-sizing :auto
-    :font-style :normal}])
+  [:html {:height "100%"}
+   [:body
+    {:background-size "cover"
+     :background-repeat :no-repeat
+     :background-image "url(gradient.svg)"
+     :font-family "Jost, sans-serif"
+     :font-optical-sizing :auto
+     :font-style :normal
+     :margin "0"
+     :height "100%"}
+    [:div.ant-select
+     {:width "100%"}]]])
 
 (defclass level1
   []
