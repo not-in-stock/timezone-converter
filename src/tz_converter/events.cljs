0bb6d85eba2273e44c42d21c2b62a4951d9a2726
Andrey Fedorov <oran9e.red@gmail.com>
2024-04-11 19:58:14 +0200

    Add events for setting timezone and time to app-db

--- src/tz_converter/events.cljs
+++ src/tz_converter/events.cljs
@@ -8,3 +8,17 @@
  ::initialize-db
  (fn-traced [_ _]
    db/default-db))
+
+(re-frame/reg-event-db
+ ::set-time
+ (fn [db [_ panel-id time]]
+   (-> db
+       (assoc-in [panel-id :time] time)
+       (assoc :primary-panel panel-id))))
+
+(re-frame/reg-event-db
+ ::set-timezone
+ (fn [db [_ panel-id timezone]]
+   (-> db
+       (assoc-in [panel-id :timezone] timezone)
+       (assoc :primary-panel panel-id))))
