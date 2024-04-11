8637ee820fa88c234640db53a1805a6028778e37
Andrey Fedorov <oran9e.red@gmail.com>
2024-04-11 16:37:10 +0200

    Remove unused styles

--- src/tz_converter/styles.cljs
+++ src/tz_converter/styles.cljs
@@ -7,7 +7,6 @@
    [:body
     {:background-size "cover"
      :background-repeat :no-repeat
-     :background-image "url(gradient.svg)"
      :font-family "Jost, sans-serif"
      :font-optical-sizing :auto
      :font-style :normal
@@ -20,8 +19,7 @@
     [:div.ant-picker
      {:width "100%"}]
     [:div.ant-divider
-     {:height "100%"
-      :border-inline-start "1px solid rgba(250, 250, 250, 0.09)"}]]])
+     {:height "100%"}]]])
 
 (defclass heading
   []
