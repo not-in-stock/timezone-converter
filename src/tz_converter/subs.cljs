e886fd36adf473a4fdc6bab00b39b70abac1d9d9
Andrey Fedorov <oran9e.red@gmail.com>
2024-04-11 19:09:06 +0200

    Add default values subs

--- src/tz_converter/subs.cljs
+++ src/tz_converter/subs.cljs
@@ -1,8 +1,26 @@
 (ns tz-converter.subs
   (:require
-   [re-frame.core :as re-frame]))
+   [tz-converter.util :as util]
+   [re-frame.core :as re-frame]
+   [dayjs]))
 
 (re-frame/reg-sub
  ::name
  (fn [db]
    (:name db)))
+
+(def timezones
+  (util/generate-zone-ids))
+
+(re-frame/reg-sub
+ ::default-timezone
+ (fn [_ _]
+   (let [current-timezone (util/get-current-timezone)]
+     (when (contains? timezones current-timezone)
+       current-timezone))))
+
+(re-frame/reg-sub
+ ::default-time
+ (fn [_ _]
+   (let [now-fn dayjs]
+     (now-fn))))
