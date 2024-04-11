59bf7bac145e19766166bc2202f2e86ec897a73d
Andrey Fedorov <oran9e.red@gmail.com>
2024-04-11 19:08:42 +0200

    Add re-frame shortcuts

--- src/tz_converter/util.cljs
+++ src/tz_converter/util.cljs
@@ -1,2 +1,12 @@
 (ns tz-converter.util
+  (:require [re-frame.core :as re-frame])
   (:require-macros [tz-converter.util]))
+
+(defn get-current-timezone []
+  (-> js/Intl
+      .DateTimeFormat
+      .resolvedOptions
+      .-timeZone))
+
+(def <sub (comp deref re-frame/subscribe))
+(def >evt re-frame/dispatch)
