926e0ce0c98121e52698937350c52cbf1efa734f
Andrey Fedorov <oran9e.red@gmail.com>
2024-04-12 19:29:37 +0200

    Remove unused

--- test/tz_converter/core_test.cljs
+++
@@ -1,7 +0,0 @@
-(ns tz-converter.core-test
-  (:require [cljs.test :refer-macros [deftest testing is]]
-            [tz-converter.core :as core]))
-
-(deftest fake-test
-  (testing "fake description"
-    (is (= 1 2))))
