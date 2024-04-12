11ceccb4d6b2746ffcb56cf4cc8b38fd095d5a47
Andrey Fedorov <oran9e.red@gmail.com>
2024-04-12 06:05:05 +0200

    Add default time for left panel

--- src/tz_converter/db.cljs
+++ src/tz_converter/db.cljs
@@ -1,7 +1,9 @@
 (ns tz-converter.db
-  (:require [tz-converter.util :as util]))
+  (:require [tz-converter.util :as util]
+            [tick.core :as t]))
 
 (def default-db
   {:source-panel :left-panel
    :right-panel {:timezone "UTC"}
-   :left-panel {:timezone (util/get-current-timezone)}})
+   :left-panel {:time (t/now)
+                :timezone (util/get-current-timezone)}})
