ff8300b10c668bc4164a0b8df55946f33959a59a
Andrey Fedorov <oran9e.red@gmail.com>
2024-04-12 05:31:27 +0200

    Fix util methods

--- src/tz_converter/db.cljs
+++ src/tz_converter/db.cljs
@@ -1,4 +1,6 @@
-(ns tz-converter.db)
+(ns tz-converter.db
+  (:require [tz-converter.util :as util]))
 
 (def default-db
-  {:name "re-frame"})
+  {:source-panel :left-panel
+   :left-panel {:timezone (util/get-current-timezone)}})
--- src/tz_converter/util.cljs
+++ src/tz_converter/util.cljs
@@ -1,14 +1,13 @@
 (ns tz-converter.util
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            ["@js-joda/timezone"]
+            [cljc.java-time.zone-id :as zone-id]))
 
 (defn get-current-timezone []
-  (-> js/Intl
-      .DateTimeFormat
-      .resolvedOptions
-      .-timeZone))
+  (str (zone-id/system-default)))
 
 (defn get-available-timezones []
-  (set (js/Intl.supportedValuesOf "timeZone")))
+  (set (zone-id/get-available-zone-ids)))
 
 (def <sub (comp deref re-frame/subscribe))
 
